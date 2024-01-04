d71d93e38bbb17da0cf466f33c5f95755944c679
maikCyphlock <maikolaguilar656@gmail.com>
2024-01-03 20:17:51 -0400

    🎉 feat: my new portfolio with a minimalism design 2024

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+import { astroImageTools } from "astro-imagetools";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sentry(), spotlightjs(), tailwind(), astroImageTools]
+});
