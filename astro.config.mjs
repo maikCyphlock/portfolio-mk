29bc9864d024c602956f89b54e8de5be16a31eaf
maikCyphlock <maikolaguilar656@gmail.com>
2024-01-08 14:23:52 -0400

    ✨ feat: finished new UI design

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import lighthouse from 'astro-lighthouse';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),lighthouse()]
+  integrations: [tailwind(), lighthouse()]
 });
