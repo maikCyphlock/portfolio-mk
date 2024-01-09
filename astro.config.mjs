bf873aa00d800f31a34ba3b83ef2860d73ee64d5
maikCyphlock <maikolaguilar656@gmail.com>
2024-01-08 20:33:21 -0400

    🔥 remove dead code on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import lighthouse from 'astro-lighthouse';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [tailwind(), lighthouse()]
+  integrations: [tailwind()]
 });
